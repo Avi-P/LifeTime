64133987ea9f0c846d17b153595c2d9910ffc291
Avi-P <apooruli@gmail.com>
2019-07-01 21:08:47 -0700

    Set up react-router

---
+++ src/Components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar'
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <>
+
+            <h1> Test </h1>
+
+        </>
+    }
+
+}
+
+export default NavBar;
+
