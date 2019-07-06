ff6d7ddeac453a8b48636ec6d87fb8e0f7afc834
Avi-P <apooruli@gmail.com>
2019-07-05 23:39:42 -0700

    Change donut chart code to support JSON data and implement lifecycle methods to handle rendering different donut charts

--- src/Pages/Calendar/CalendarView.js
+++ src/Pages/Calendar/CalendarView.js
@@ -3,8 +3,6 @@ import React from "react"
 import NavBar from "../../Components/NavBar"
 import Calendar from 'react-calendar';
 
-import * as d3 from "d3";
-
 import DonutSummary from "./DonutSummary"
 
 import "./CalendarView.css"
@@ -15,27 +13,40 @@ class CalendarView extends React.Component {
 
         this.onChange = this.onChange.bind(this);
 
-        let dataX = this.generateData();
-
         this.state = {
+            firstInitialize: false,
             date: new Date(),
-            data: [5,6,7,8],
+            data: [],
             width: 900,
-            height: 550,
+            height: 620,
             id: "root"
         }
     }
 
-    generateData (value, length = 5){
-        d3.range(length).map((item, index) => ({
-            date: index,
-            value: value === null || value === undefined ? Math.random() * 100 : value
-        }))};
+    componentDidMount() {
+        let data = [];
+
+        data.push(JSON.parse('{"time_in" : 0, "time_out" : 12, "name" : "Sleep"}'));
+        data.push(JSON.parse('{"time_in" : 12, "time_out" : 13, "name" : "Class"}'));
+        data.push(JSON.parse('{"time_in" : 13, "time_out" : 14, "name" : "Eating"}'));
+        data.push(JSON.parse('{"time_in" : 14, "time_out" : 19, "name" : "Class"}'));
+        data.push(JSON.parse('{"time_in" : 19, "time_out" : 24, "name" : "Work"}'));
+
+        this.setState({
+            data: data
+        })
+    }
 
     onChange(date) {
         this.setState({
             date: date
         })
+    }
+
+    generateChart() {
+        let donut =  (<DonutSummary id="forRemove" date={this.state.date} width={this.state.width} height={this.state.height} />);
+
+        return donut;
     }
 
     render() {
@@ -50,22 +61,20 @@ class CalendarView extends React.Component {
                                 </div>
                                 <h3> Day Overview </h3>
                                 <br></br>
-                                <h4> 37.5% - Sleeping</h4>
-                                <h4> 16.7% - Class</h4>
-                                <h4> 16.7% - Eating</h4>
-                                <h4> 29.2% - Work</h4>
+                                <h5> 37.5% - Sleeping</h5>
+                                <h5> 16.7% - Class</h5>
+                                <h5> 16.7% - Eating</h5>
+                                <h5> 29.2% - Work</h5>
                             </center>
                         </div>
 
                         <div id = "main">
-                                <DonutSummary data={this.state.data} width={this.state.width} height={this.state.height} />
+                            {this.generateChart()}
                         </div>
-
                     </div>
             </div>
         )
     }
-
 }
 
 export default CalendarView
--- src/Pages/Calendar/DonutSummary.js
+++ src/Pages/Calendar/DonutSummary.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import { scaleOrdinal } from 'd3-scale';
 import { arc as d3Arc, pie as d3Pie } from 'd3-shape';
-import { csvParse } from 'd3-dsv';
 
 class DonutSummary extends React.Component{
 
@@ -10,9 +9,10 @@ class DonutSummary extends React.Component{
         super(props);
 
         this.state = {
+            data: [],
             width: this.props.width,
             height: this.props.height,
-            radius: Math.min(this.props.width, this.props.height) / 2,
+            radius: Math.min(this.props.width, this.props.height) / 2.5,
             colors: scaleOrdinal().range([
                                                 '#3c6fc2',
                                                 '#8a89a6',
@@ -22,8 +22,63 @@ class DonutSummary extends React.Component{
                                                 '#d0743c',
                                                 '#ff8c00',
                                             ]),
+        };
+    }
 
+    generateRandomData() {
+        let data = [];
+        let activity = ["Sleep", "Work", "Class", "Food", "Exercise"];
+        let startNumber = 0;
+        let rand = Math.floor(Math.random() * 5) + 1;
+
+        for(let i = 0; i < rand; i++) {
+            let num = startNumber;
+            let numTwo = Math.floor(Math.random() * 24 - num) + num;
+            startNumber = numTwo;
+
+            let numThree =  Math.floor(Math.random() * 4);
+
+            let obj = '{"time_in" :' + num.toString() + ', "time_out" :' + numTwo.toString() + ', "name" : "' + activity[i] + '"}';
+
+            data.push(JSON.parse(obj));
         }
+
+        return data;
+    }
+
+    componentDidMount() {
+
+        // data.push(JSON.parse('{"time_in" : 0, "time_out" : 9, "name" : "Sleep"}'));
+        // data.push(JSON.parse('{"time_in" : 9, "time_out" : 12, "name" : "Class"}'));
+        // data.push(JSON.parse('{"time_in" : 12, "time_out" : 14, "name" : "Eating"}'));
+        // data.push(JSON.parse('{"time_in" : 14, "time_out" : 15, "name" : "Class"}'));
+        // data.push(JSON.parse('{"time_in" : 15, "time_out" : 19, "name" : "Work"}'));
+        // data.push(JSON.parse('{"time_in" : 19, "time_out" : 21, "name" : "Eating"}'));
+        // data.push(JSON.parse('{"time_in" : 21, "time_out" : 24, "name" : "Work"}'));
+
+        this.setState({
+            data: this.generateRandomData()
+        })
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.shouldUpdate(prevProps, this.props)) {
+           this.update();
+        }
+    }
+
+    shouldUpdate(prevProps, props) {
+        if (prevProps.date !== props.date) {
+            return true;
+        }
+
+        return false;
+    }
+
+    update() {
+        this.setState({
+            data: this.generateRandomData(),
+        });
     }
 
     midAngle(d){
@@ -31,15 +86,14 @@ class DonutSummary extends React.Component{
     }
 
     render() {
-        let dataTest =
-            `time,name
-            540,Sleeping
-            180,Class
-            120,Eating
-            60,Class
-            240,Work
-            120,Eating
-            180,Work`;
+        let dataCleaned = [];
+
+        for (let i = 0; i < this.state.data.length; i++) {
+            let cleaned = '{"time" :' + (this.state.data[i]["time_out"] - this.state.data[i]["time_in"])
+                            + ', "activity": "' + this.state.data[i]["name"] + '"}';
+
+            dataCleaned.push(JSON.parse(cleaned));
+        }
 
         const arc = d3Arc()
             .outerRadius(this.state.radius - 10)
@@ -52,46 +106,37 @@ class DonutSummary extends React.Component{
 
         const pie = d3Pie()
             .sort(null)
-            .value(function(d) {
-                return d.time//d.population;
-            });
+            .value(function(d) { return d.time; });
 
-        const data = pie( //dataTest
-            csvParse(dataTest, d => {
-                d.time = +d.time;
-                return d;
-            })
-        );
+        const data = pie(dataCleaned);
 
         return (
-            <svg id = "visual" width={this.state.width} height={this.state.height}>
-                <g transform={`translate(${this.state.width / 2}, ${this.state.height / 2})`}>
-                    {data.map(d => {
-                        let SVGpos = outerArc.centroid(d);
-                        let textPos = outerArc.centroid(d);
+                <svg id = "visual" width={this.state.width} height={this.state.height}>
+                    <g transform={`translate(${this.state.width / 2}, ${this.state.height / 2})`}>
+                        {data.map(d => {
+                            let SVGpos = outerArc.centroid(d);
+                            let textPos = outerArc.centroid(d);
 
-                        SVGpos[0] = this.state.radius * .90 * (this.midAngle(d) < Math.PI ? 1 : -1);
-                        textPos[0] = this.state.radius * (this.midAngle(d) < Math.PI ? 1 : -1);
+                            SVGpos[0] = this.state.radius * .90 * (this.midAngle(d) < Math.PI ? 1 : -1);
+                            textPos[0] = this.state.radius * (this.midAngle(d) < Math.PI ? 1 : -1);
 
-                        let retString = arc.centroid(d)+ " " + outerArc.centroid(d) + " " + textPos;
+                            let retString = arc.centroid(d)+ " " + outerArc.centroid(d) + " " + textPos;
 
-                        //console.log(d.data.name + " " + retString);
+                            return (
+                                <g className="arc" key={`a${d.data.activity}`}>
+                                    <path d={arc(d)} fill={this.state.colors(d.data.activity)} />
+                                    <polyline points={retString} fill="none" stroke="black" strokeWidth= "1px" opacity = ".3"/>
 
-                        return (
-                            <g className="arc" key={`a${d.data.time}`}>
-                                <path d={arc(d)} fill={this.state.colors(d.data.time)} />
-                                <polyline points={retString} fill="none" stroke="black" stroke-width= "1px" opacity = ".3"/>
+                                    {(textPos[0] < 0 ? textPos[0] = textPos[0] * 1.2 : textPos[0])}
 
-                                {(textPos[0] < 0 ? textPos[0] = textPos[0] * 1.19 : textPos[0])}
-
-                                <text transform={`translate(${textPos/*arc.centroid(d)*/})`} dy=".35em">
-                                    {d.data.name}
-                                </text>
-                            </g>
-                        )
-                    })}
-                </g>
-            </svg>
+                                    <text transform={`translate(${textPos/*arc.centroid(d)*/})`} dy=".35em">
+                                        {d.data.activity}
+                                    </text>
+                                </g>
+                            )
+                        })}
+                    </g>
+                </svg>
         );
     }
 }
